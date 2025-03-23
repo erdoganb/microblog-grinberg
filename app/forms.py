07182acaff7073a5fbc767e29d2493a3163a38e2
erdoganb <erdoganborklu@gmail.com>
2025-03-23 15:53:51 +0300

    profile page and avatars

--- app/forms.py
+++ app/forms.py
@@ -7,6 +7,9 @@ from app import db
 from app.models import User
 import sqlalchemy as sa
 
+from wtforms import TextAreaField
+from wtforms.validators import Length
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -29,3 +32,9 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.select(User). where(User.email == email.data))
         if user is not None:
             raise ValidationError('Please use a different email.')
+        
+    
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 
+from hashlib import md5
+
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
@@ -19,7 +21,6 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self):
@@ -30,6 +31,15 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+    
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -42,3 +52,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
