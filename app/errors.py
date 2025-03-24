cbef71b878d0a211065d1ea7adb0e8c2356b410f
erdoganb <erdoganborklu@gmail.com>
2025-03-24 13:27:59 +0300

    error handling retouch

--- app/errors.py
+++ app/errors.py
@@ -7,7 +7,7 @@ def page_not_found(error):
     print("anan")
     return render_template('404.html'), 404
 
-@app.errorhandler
+@app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template(url_for('index')), 500
+    return render_template('500.html'), 500
