303082136bc5558ac1846a6879d00cc9ca99a502
erdoganb <erdoganborklu@gmail.com>
2025-03-24 12:10:03 +0300

    error handling

---
+++ app/errors.py
@@ -0,0 +1,13 @@
+from app import app, db
+from flask import render_template, url_for
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    print("anan")
+    return render_template('404.html'), 404
+
+@app.errorhandler
+def internal_error(error):
+    db.session.rollback()
+    return render_template(url_for('index')), 500
