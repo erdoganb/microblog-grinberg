8a9c304fb81c9147985b4fb13ed1975db0b8409f
erdoganb <erdoganborklu@gmail.com>
2025-03-25 14:31:53 +0300

    error handling, error log, error mail

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,9 @@ login.login_view = 'login'
 import logging
 from logging.handlers import SMTPHandler
 
+from logging.handlers import RotatingFileHandler
+import os
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -34,4 +37,15 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
     
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
 from app import routes, models, errors
