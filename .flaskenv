2d28de021b074f4997b6a2a553ce608d16d70911
erdoganb <erdoganborklu@gmail.com>
2025-03-25 12:17:14 +0300

    error handling mail

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=microblog.py
 FLASK_DEBUG=0
+MAIL_USERNAME=reisov80@gmail.com
--- config.py
+++ config.py
@@ -10,3 +10,10 @@ class Config:
 #print('sqlite:///' + os.path.join(basedir, 'app.db'))
 #database path
 #sqlite:////Users/erdoganb/Desktop/erdo-dev/PY/microblog-grinberg/app.db
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['reisov80@gmail.com']
